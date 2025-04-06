630bbcbbdf89d6f5d55179c9ebdaace749e1281c
Nobita13349 <nobita13349@gmail.com>
2025-04-06 16:55:52 +0530

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -3,3 +3,13 @@ window.Telegram.WebApp.ready();
 document.getElementById("betButton").addEventListener("click", function () {
     Telegram.WebApp.sendData("User wants to place a bet!");
 });
+
+window.Telegram.WebApp.ready();
+
+document.getElementById("teamA").addEventListener("click", function () {
+    Telegram.WebApp.sendData("User selected Team A for the bet!");
+});
+
+document.getElementById("teamB").addEventListener("click", function () {
+    Telegram.WebApp.sendData("User selected Team B for the bet!");
+});
